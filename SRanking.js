154cf3952c786b7ba1d90aaef6e178a24bb1b4c0
a449332413 <449332413@qq.com>
2015-12-20 14:13:52 +0800

    Update SRanking.js

--- SRanking.js
+++ SRanking.js
@@ -1,14 +1,9 @@
 // JavaScript Document
 (function() {
- 	if (!document.domain) {
- 		alert("或不支持当前浏览器，请换其他浏览器尝试！");
- 		return
- 	}
  	if (document.domain.toLowerCase().indexOf("s.taobao.com") < 0) {
- 		alert("不支持当前网站！");
+ 		alert("请在淘宝搜索页面(s.taobao.com)下进行！");
  		return
  	}
-	alert("ok!");
-	alert(window.location.href);
+	$("body").append("<div style='z-index: 999999;position: fixed;height: 50px; bottom: 0px;margin-left: 50%;'><div style='width: 1024px;height: 100%;margin-left: -512px;background-color: #fff;box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.1);border: 1px solid #E8E8E8;padding: 5px;'>加载成功！</div></div>");
 }
 )();
