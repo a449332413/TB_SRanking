b53b5724b32e63e7d063a311c6bca16b63f5a814
a449332413 <449332413@qq.com>
2015-12-20 15:59:45 +0800

    Update SRanking.js

--- SRanking.js
+++ SRanking.js
@@ -4,6 +4,8 @@
  		alert("请在淘宝搜索页面(s.taobao.com)下进行！");
  		return
  	}
-	KISSY.DOM.append(KISSY.DOM.create("<div style='z-index: 999999;position: fixed;height: 50px; bottom: 0px;margin-left: 50%;'><div style='width: 1024px;height: 100%;margin-left: -512px;background-color: #fff;box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.1);border: 1px solid #E8E8E8;padding: 5px;'>加载成功！</div></div>"), "body");
+	KISSY.getScript('https://a449332413.github.io/TB_SRanking/SRanking.css');
+	
+	KISSY.DOM.append(KISSY.DOM.create("<div id='TB_SRanking'><div class='TB_SRcontent'>加载成功！</div></div>"), "body");
 }
 )();
